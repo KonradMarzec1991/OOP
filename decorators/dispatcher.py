f54102275f2ba7703883ed5b20ca157630cbc25d
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-01 22:23:12 +0100

    Create dispatcher htmlize func

--- decorators/dispatcher.py
+++ decorators/dispatcher.py
@@ -1,4 +1,5 @@
 from html import escape
+from decimal import Decimal
 
 
 def html_escape(arg):
@@ -10,20 +11,34 @@ def html_int(a):
 
 
 def html_real(a):
-    return '{0:;2f}'.format(round(a, 2))
+    return '{0:.2f}'.format(round(a, 2))
 
 
 def html_str(a):
-    return html_escape(a).replace('\n', '<br>/\n')
+    return html_escape(a).replace('\n', '<br>\n')
 
 
 def html_list(l):
-    items = ('<li>{0}</li>'.format(html_escape(item)) for item in l)
+    items = ('<li>{0}</li>'.format(htmlize(item)) for item in l)
     return '<ul>\n' + '\n'.join(items) + '\n</ul>'
 
 
 def html_dict(d):
-    items = ('<li>{0}={1}</li>'.format(k, v) for k, v in d.items())
+    items = ('<li>{0}={1}</li>'.format(
+        html_escape(k), htmlize(v)) for k, v in d.items()
+    )
     return '<ul>\n' + '\n'.join(items) + '\n</ul>'
 
 
+def htmlize(arg):
+    if isinstance(arg, int):
+        return html_int(arg)
+    if isinstance(arg, (float, Decimal)):
+        return html_real(arg)
+    if isinstance(arg, str):
+        return html_str(arg)
+    if isinstance(arg, (list, tuple)):
+        return html_list(arg)
+    if isinstance(arg, dict):
+        return html_dict(arg)
+    return html_escape(arg)

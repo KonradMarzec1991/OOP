9a606ba450e4f6aca0ad812058f1a23fbaf0ec91
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-01 16:27:20 +0100

    Create IntegerField class

---
+++ descriptors/base_validator.py
@@ -0,0 +1,28 @@
+import numbers
+
+
+class IntegerField:
+    def __init__(self, min_, max_):
+        self._min = min_
+        self._max = max_
+
+    def __set_name__(self, owner, name):
+        self.property_name = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.property_name, None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, numbers.Integral):
+            raise ValueError(f'{self.property_name} must be an int')
+        if value < self._min:
+            raise ValueError(
+                f'{self.property_name} must be greater than {self._min}'
+            )
+        if value > self._max:
+            raise ValueError(
+                f'{self.property_name} must be lower than {self._max}'
+            )
+        instance.__dict__[self.property_name] = value

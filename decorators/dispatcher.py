a4d298adce6b152998f574f9f2fb19d867501a74
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-02 14:46:54 +0100

    Introduce registry for types

--- decorators/dispatcher.py
+++ decorators/dispatcher.py
@@ -1,58 +1,15 @@
 from html import escape
-from decimal import Decimal
 
 
 def html_escape(arg):
     return escape(str(arg))
 
 
-def html_int(a):
-    return '{0}(<i><{1}/i>)'.format(a, str(hex(a)))
-
-
-def html_real(a):
-    return '{0:.2f}'.format(round(a, 2))
-
-
-def html_str(a):
-    return html_escape(a).replace('\n', '<br>\n')
-
-
-def html_list(l):
-    items = ('<li>{0}</li>'.format(htmlize(item)) for item in l)
-    return '<ul>\n' + '\n'.join(items) + '\n</ul>'
-
-
-def html_dict(d):
-    items = ('<li>{0}={1}</li>'.format(
-        html_escape(k), htmlize(v)) for k, v in d.items()
-    )
-    return '<ul>\n' + '\n'.join(items) + '\n</ul>'
-
-
-def htmlize(arg):
-
-    registry = {
-        object: html_escape,
-        int: html_int,
-        float: html_real,
-        Decimal: html_real,
-        str: html_str,
-        list: html_list,
-        tuple: html_list,
-        set: html_list,
-        dict: html_dict,
-    }
-
-    fn = registry.get(type(arg), registry[object])
-    return fn(arg)
-
-
 def single_dispatch(fn):
     registry = {object: fn}
 
     def decorated(arg):
-        return registry.get(type(arg), registry[object])
+        return registry.get(type(arg), registry[object])(arg)
 
     def register(type_):
         def inner(fn):
@@ -66,3 +23,37 @@ def single_dispatch(fn):
     decorated.register = register
     decorated.dispatch = dispatch
     return decorated
+
+
+@single_dispatch
+def htmlize(arg):
+    return escape(str(arg))
+
+
+@htmlize.register(int)
+def html_int(a):
+    return '{0} (<i><{1}/i>)'.format(a, str(hex(a)))
+
+
+@htmlize.register(float)
+def html_real(a):
+    return '{0:.2f}'.format(round(a, 2))
+
+
+@htmlize.register(str)
+def html_str(a):
+    return html_escape(a).replace('\n', '<br>\n')
+
+
+@htmlize.register(list)
+def html_sequence(l):
+    items = ('<li>{0}</li>'.format(htmlize(item)) for item in l)
+    return '<ul>\n' + '\n'.join(items) + '\n</ul>'
+
+
+@htmlize.register(dict)
+def html_dict(d):
+    items = ('<li>{0}={1}</li>'.format(
+        html_escape(k), htmlize(v)) for k, v in d.items()
+    )
+    return '<ul>\n' + '\n'.join(items) + '\n</ul>'

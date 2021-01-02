0331143c557ce3b0be193388c372de192590cac8
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-02 19:54:09 +0100

    Reorganize imports folder

--- imports/module2.py
+++ imports/module2.py
@@ -1,27 +1,10 @@
-import math
+# module2.py
+
+print('Running module2.py')
+
+import module1
 
 
-class CustomType(type):
-    def __new__(cls, name, bases, class_dict):
-        print('Customized type creation!')
-        cls_obj = super().__new__(cls, name, bases, class_dict)
-        cls_obj.circ = lambda self: 2 * math.pi * self.r
-        return cls_obj
-
-
-class_body = """
-def __init__(self, x, y, r):
-    self.x = x
-    self.y = y
-    self.r = r
-
-def area(self):
-    return math.pi * self.r ** 2
-"""
-
-
-class_dict = {}
-exec(class_body, globals(), class_dict)
-Circle = CustomType('Circle', (), class_dict)
-c = Circle(1, 1, 2)
-print(c.__dict__)
+def hello():
+    print('Module2 says hello!\n...')
+    module1.hello()

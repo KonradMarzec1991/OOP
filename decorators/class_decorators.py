7f7fb0618c8903df46838ec929f619f9497a5492
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-03 23:28:57 +0100

    Inspect if routine

--- decorators/class_decorators.py
+++ decorators/class_decorators.py
@@ -1,4 +1,4 @@
-from functools import wraps
+import inspect
 
 
 def account_type(type_):
@@ -61,3 +61,47 @@ class Person:
 
     def great(self):
         return f'Hello my name is {self.name}, and I am {self.age} years old'
+
+
+def class_spec_logger(cls):
+
+    def func_logger(fn):
+        def inner(*args, **kwargs):
+            result = fn(*args, **kwargs)
+            print(f'Log: {fn.__qualname__} ({args}, {kwargs}) = {result}')
+            return result
+
+        return inner
+
+    for name, obj in vars(cls).items():
+        if isinstance(obj, staticmethod):
+            print('decorating static method', cls, name)
+            original_func = obj.__func__
+            decorated_func = func_logger(original_func)
+            method = staticmethod(decorated_func)
+            setattr(cls, name, method)
+
+        elif isinstance(obj, classmethod):
+            print('decorating callable', cls, name)
+            original_func = obj.__func__
+            decorated_func = func_logger(original_func)
+            method = classmethod(decorated_func)
+            setattr(cls, name, method)
+
+        elif isinstance(obj, property):
+            print('decorating property', cls, name)
+            if obj.fget:
+                obj = obj.getter(func_logger(obj.fget))
+            if obj.fset:
+                obj = obj.setter(func_logger(obj.fset))
+            if obj.fdel:
+                obj = obj.deleter(func_logger(obj.fdel))
+            setattr(cls, name, obj)
+
+        elif inspect.isroutine(obj):
+            print('decorating callable', cls, name)
+            original_func = obj
+            decorated_func = func_logger(original_func)
+            setattr(cls, name, decorated_func)
+
+    return cls

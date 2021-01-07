ff41a2c1ea86b8384f9df73e801450437976ce25
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-07 17:28:45 +0100

    Add meta decorator

--- metaclasses/metaclass_application.py
+++ metaclasses/metaclass_application.py
@@ -1,20 +1,3 @@
-class Point2D:
-    _fields = ['x', 'y']
-
-    def __init__(self, x, y):
-        self._x = x
-        self._y = y
-
-
-class Point3D:
-    _fields = ['x', 'y', 'z']
-
-    def __init__(self, x, y, z):
-        self._x = x
-        self._y = y
-        self._z = z
-
-
 class SlottedStruct(type):
     def __new__(mcls, name, bases, cls_dict):
         cls_object = super().__new__(mcls, name, bases, cls_dict)
@@ -39,7 +22,7 @@ class SlottedStruct(type):
         def eq(self, other):
             if isinstance(other, cls_object):
                 self_fields = [
-                    getattr(self, field) for field in cls_object._fields
+                    getattr(self, field) for field in self._fields
                 ]
                 other_fields = [
                     getattr(self, field) for field in other._fields
@@ -52,7 +35,7 @@ class SlottedStruct(type):
         # __hash__
         def hash_(self):
             field_values = [
-                    getattr(self, field) for field in cls_object._fields
+                    getattr(self, field) for field in self._fields
                 ]
             return hash(tuple(field_values))
 
@@ -61,21 +44,51 @@ class SlottedStruct(type):
         # __str__
         def str_(self):
             field_values = [
-                getattr(self, field) for field in cls_object._fields
+                getattr(self, field) for field in self._fields
             ]
             field_values_joined = ', '.join(map(str, field_values))
             return f'{cls_object.__name__}({field_values_joined})'
 
         setattr(cls_object, '__str__', str_)
 
+        # __repr__
+        def repr_(self):
+            field_values = [
+                getattr(self, field) for field in self._fields
+            ]
+            field_key_values = (
+                f'{key}={value}' for key, value in zip(self._fields, field_values)
+            )
+            field_key_values_str = ', '.join(field_key_values)
+            return f'{self.__class__.__name__}({field_key_values_str})'
 
+        setattr(cls_object, '__repr__', repr_)
 
         return cls_object
 
 
-class Person(metaclass=SlottedStruct):
-    _fields = ['name', 'age']
+def struct(cls):
+    return SlottedStruct(
+        cls.__name__,
+        cls.__bases__,
+        dict(cls.__dict__)
+    )
 
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
+
+@struct
+class Point2D:
+    _fields = ['x', 'y']
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+
+@struct
+class Point3D:
+    _fields = ['x', 'y', 'z']
+
+    def __init__(self, x, y, z):
+        self._x = x
+        self._y = y
+        self._z = z

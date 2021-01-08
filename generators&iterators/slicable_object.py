2d751238a771b85d151c45954f754fbd161b3b30
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-08 23:53:16 +0100

    Add len/repr method

--- generators&iterators/slicable_object.py
+++ generators&iterators/slicable_object.py
@@ -1,14 +1,28 @@
+from array import array
+import reprlib
 import numbers
 
 
 class Vector:
+    type_code = 'd'
+
     def __init__(self, components):
-        self._components = components
+        self._components = array(self.type_code, components)
 
     def __getitem__(self, key):
+        cls = type(self)
         if isinstance(key, slice):
-            pass
+            return cls(self._components[key])
         elif isinstance(key, numbers.Integral):
-            pass
+            return self._components[key]
         else:
-            pass
+            msg = '{cls.__name__} indices must be integers'
+            raise TypeError(msg.format(cls=cls))
+
+    def __len__(self):
+        return len(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return 'Vector({})'.format(components)

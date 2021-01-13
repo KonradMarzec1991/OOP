0efe126f0645e11fee23d5e1333be04b4634371e
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-13 21:35:02 +0100

    Reflect the presence of debug argument in wrapper

--- decorators/logger_decorator.py
+++ decorators/logger_decorator.py
@@ -69,4 +69,13 @@ def optional_debug(func):
         if debug:
             print('Calling', func.__name__)
         return func(*args, **kwargs)
+
+    sig = inspect.signature(func)
+    parms = list(sig.parameters.values())
+    parms.append(inspect.Parameter(
+        'debug',
+        inspect.Parameter.KEYWORD_ONLY,
+        default=False
+    ))
+    wrapper.__signature__ = sig.replace(parameters=parms)
     return wrapper

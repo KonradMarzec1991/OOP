32536a6ca0dc6836253878e9ddc49917fa36226f
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-29 21:31:56 +0100

    Add inheritance

--- JavaScript/oop/assets/scripts/app.js
+++ JavaScript/oop/assets/scripts/app.js
@@ -17,6 +17,10 @@ class ElementAttribute {
 
 
 class Component {
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
+  }
+
   createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
     if (cssClasses) {
@@ -27,11 +31,13 @@ class Component {
         rootElement.setAttribute(attr.name, attr.value);
       })
     }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
   }
 }
 
 
-class ShoppingCart {
+class ShoppingCart extends Component {
   items = [];
 
   set cartItems(value) {
@@ -47,6 +53,11 @@ class ShoppingCart {
     return sum;
   }
 
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
+
+
   addProduct(product) {
     const updatedItems = [...this.items];
     updatedItems.push(product);
@@ -54,14 +65,12 @@ class ShoppingCart {
   }
 
   render() {
-    const cartEl = document.createElement('section');
+    const cartEl = this.createRootElement('section', 'cart')
     cartEl.innerHTML = `
       <h2>Total: \$${0}</h2>
       <button>Order Now!</button>
     `;
-    cartEl.className = 'cart';
     this.totalOutput = cartEl.querySelector('h2');
-    return cartEl;
   }
 }
 
@@ -128,12 +137,11 @@ class Shop {
   render() {
     const renderHook = document.getElementById('app');
 
-    this.cart = new ShoppingCart();
-    const cartEl = this.cart.render();
+    this.cart = new ShoppingCart('app');
+    this.cart.render();
     const productList = new ProductList();
     const prodListEl = productList.render();
 
-    renderHook.append(cartEl);
     renderHook.append(prodListEl);
   }
 }

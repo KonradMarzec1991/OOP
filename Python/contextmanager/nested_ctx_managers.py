88916f5c9b6ea2f9a1d5d9ffa003ffd457fb040b
KonradMarzec1991 <konrimarzec@gmail.com>
2021-05-06 21:13:52 +0200

    Test NestedContextManager

--- Python/contextmanager/nested_ctx_managers.py
+++ Python/contextmanager/nested_ctx_managers.py
@@ -1,3 +1,6 @@
+from ctx_decorator import open_file
+
+
 class NestedContextManager:
     def __init__(self, *contexts):
         self._enters = []
@@ -17,3 +20,17 @@ class NestedContextManager:
         for ext in self._exits[::-1]:
             ext(exc_type, exc_val, exc_tb)
         return False
+
+
+with NestedContextManager(
+    open_file('file1.txt', 'r'),
+    open_file('file2.txt', 'r')
+) as files:
+    while True:
+        try:
+            rows = [next(f).strip('\n') for f in files]
+        except StopIteration:
+            break
+        else:
+            row = ','.join(rows)
+            print(row)

81fd1becf5a06f8db9ffa74b70834804705d8a43
KonradMarzec1991 <konrimarzec@gmail.com>
2021-05-06 20:59:29 +0200

    Add GenContextManager

--- Python/contextmanager/ctx_decorator.py
+++ Python/contextmanager/ctx_decorator.py
@@ -6,3 +6,25 @@ def open_file(f_name, mode='r'):
     finally:
         print(f'closing {f_name} file')
         f.close()
+
+
+class GenContextManager:
+    def __init__(self, gen):
+        self._gen = gen
+
+    def __enter__(self):
+        print('calling next to perform yielded value from generator')
+        return next(self._gen)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('calling next to perform cleanup in generator')
+        try:
+            next(self._gen)
+        except StopIteration:
+            pass
+        return False
+
+
+file_gen = open_file('test.txt', 'r')
+with GenContextManager(file_gen) as f:
+    f.writelines('abcd')

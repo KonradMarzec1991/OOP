542ce36a17d187fd524e3edffa483ab237d5fe75
KonradMarzec1991 <konrimarzec@gmail.com>
2021-05-09 20:28:45 +0200

    Add get_dialect func, start FileParser class

--- Python/contextmanager/project/ctx.py
+++ Python/contextmanager/project/ctx.py
@@ -13,3 +13,17 @@ for i in (0, 1):
     with open(f_names[i]) as f:
         dialect = csv.Sniffer().sniff(f.read(1000))
         print(dialect.delimiter)
+
+
+def get_dialect(f_name):
+    with open(f_name) as f:
+        return csv.Sniffer().sniff(f.read(1000))
+
+
+class FileParser:
+    def __init__(self, f_name):
+        self.f_name = f_name
+
+    def __enter__(self):
+        self._f = open(self.f_name, 'r')
+        self._reader = csv.reader(self.f_name, dialect=get_dialect(self.f_name))

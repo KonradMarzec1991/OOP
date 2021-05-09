6775ff43582b6183f6c992fd6e3e45e8006a1dbd
KonradMarzec1991 <konrimarzec@gmail.com>
2021-05-09 21:00:21 +0200

    Add parsed_data

--- Python/contextmanager/project/ctx.py
+++ Python/contextmanager/project/ctx.py
@@ -1,6 +1,8 @@
 import csv
 from collections import namedtuple
+from contextlib import contextmanager
 from itertools import islice
+
 
 f_names = 'cars.csv', 'personal_info.csv'
 
@@ -38,3 +40,17 @@ class FileParser:
 with FileParser('cars.csv') as data:
     for row in islice(data, 10):
         print(row)
+
+
+@contextmanager
+def parsed_data(f_name):
+    f = open(f_name, mode='r')
+    try:
+        dialect = csv.Sniffer().sniff(f.read(1000))
+        f.seek(0)
+        reader = csv.reader(f, dialect)
+        headers = map(lambda s: s.lower(), next(reader))
+        nt = namedtuple('Data', headers)
+        yield (nt(*row) for row in headers)
+    finally:
+        f.close()

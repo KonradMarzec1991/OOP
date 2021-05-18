192a8ab8afc9f2d4611c5c55b37b6fe45a7471f7
KonradMarzec1991 <konrimarzec@gmail.com>
2021-05-18 20:19:40 +0200

    Add CardDeck class

--- Python/iterators&generators/card_deck.py
+++ Python/iterators&generators/card_deck.py
@@ -1,18 +1,26 @@
 from collections import namedtuple
 
-
 Card = namedtuple('Card', 'rank suit')
-SUITS = ('Spades', 'Heats', 'Diamonds', 'Clubs')
-RANKS = tuple(range(2, 11)) + tuple('JQKA')
 
 
-def card_gen():
-    for i in range(len(SUITS) * len(RANKS)):
-        suit = SUITS[i // len(RANKS)]
-        rank = RANKS[i % len(RANKS)]
-        card = Card(rank, suit)
-        yield card
+class CardDeck:
+    SUITS = ('Spades', 'Heats', 'Diamonds', 'Clubs')
+    RANKS = tuple(range(2, 11)) + tuple('JQKA')
 
+    def __iter__(self):
+        return CardDeck.card_gen()
 
-for card in card_gen():
-    print(card)
+    def __reversed__(self):
+        return CardDeck.reversed_card_gen()
+
+    @staticmethod
+    def card_gen():
+        for suit in CardDeck.SUITS:
+            for rank in CardDeck.RANKS:
+                yield Card(rank, suit)
+
+    @staticmethod
+    def reversed_card_gen():
+        for suit in reversed(CardDeck.SUITS):
+            for rank in reversed(CardDeck.RANKS):
+                yield Card(rank, suit)

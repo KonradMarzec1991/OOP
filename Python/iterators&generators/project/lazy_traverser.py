b3430fa891118b6781f6096793f6adc80aa2132d
KonradMarzec1991 <konrimarzec@gmail.com>
2021-05-22 19:38:39 +0200

    Add parsers

--- Python/iterators&generators/project/lazy_traverser.py
+++ Python/iterators&generators/project/lazy_traverser.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
-import csv
-
+from functools import partial
+from datetime import datetime
 
 
 def open_file(file_name: str):
@@ -22,3 +22,41 @@ def open_file(file_name: str):
 
 
 open_file('nyc_parking_tickets_extract.csv')
+
+
+def parse_int(value, *, default=None):
+    try:
+        return int(value)
+    except ValueError:
+        return default
+
+
+def parse_date(value, *, default=None):
+    date_format = '%m/%d/%Y'
+    try:
+        return datetime.strptime(value, date_format).date()
+    except ValueError:
+        return default
+
+
+def parse_str(value, *, default=None):
+    try:
+        cleaned = value.strip()
+        if not cleaned:
+            return default
+        return cleaned
+    except ValueError:
+        return default
+
+
+column_parser = (
+    parse_int,
+    parse_str,
+    partial(parse_str, default=''),
+    partial(parse_str, default=''),
+    parse_date,
+    parse_int,
+    partial(parse_str, default=''),
+    parse_str,
+    partial(parse_str, default='')
+)
